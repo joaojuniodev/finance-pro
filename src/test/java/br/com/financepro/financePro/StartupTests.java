65a221c50c3b80dec91cb7ded8a0a461f48363af
JoaoJunio09 <joaojunio818@gmail.com>
2026-06-14 11:09:46 -0300

    initial commit

---
+++ src/test/java/br/com/financepro/financePro/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.financepro.financePro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
